1e1724d2ee810253fe181da9af5b2f76e1eeba2b
MrSagarBiswas <biswassagar927@gmail.com>
2024-05-10 18:47:30 +0530

    tag

--- client/src/ui/Footer.jsx
+++ client/src/ui/Footer.jsx
@@ -12,10 +12,10 @@ export default function Footer() {
 				<ul className="flex mt-6 justify-center md:justify-start space-x-4">
 					<li>
 						<Link to="https://www.linkedin.com/in/mrsagarbiswas/" target="_blank"><Linkedin /></Link>
-					</li>				
+					</li>
 					<li>
 						<Link to="https://www.instagram.com/mrsagarbiswas/" target="_blank"><Instagram /></Link>
-					</li>				
+					</li>
 					<li>
 						<a href="https://github.com/MrSagarBiswas" target='_blank'><GitHub /></a>
 					</li>
@@ -23,7 +23,7 @@ export default function Footer() {
 						<a href="https://www.google.com/search?q=MrSagarBiswas&rlz=1C1CHBF_enIN970IN970&oq=MrSagarBiswas&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgoIARAAGIAEGKIEMgYIAhBFGDwyBggDEEUYPDIGCAQQRRg80gEINDA4MWowajeoAgCwAgA&sourceid=chrome&ie=UTF-8#ip=1" target="_blank"><Globe /></a>
 					</li>
 				</ul>
-			</div>			
+			</div>
 			<div className="m-4 sm:m-6">
 				<h2 className="text-xl text-center md:text-left font-medium mb-4">Useful Links</h2>
 				<ul className="flex flex-col flex-wrap h-36 space-y-1 text-gray-600">
@@ -44,30 +44,30 @@ export default function Footer() {
 					</li>
 					<li>
 						<Link to="/account">My Account</Link>
-					</li>							
+					</li>
 					<li>
 						<Link to="/wishlist">Wishlist</Link>
-					</li>					
+					</li>
 					<li>
 						<Link to="/terms">Terms</Link>
 					</li>
 				</ul>
-			</div>			
+			</div>
 			<div className="m-4 sm:m-6">
 				<h2 className="text-xl text-center md:text-left font-medium mb-4">Contact</h2>
 				<ul className="space-y-3 text-gray-700">
 					<li className="flex items-center">
 						<MapPin className="w-5 h-5 mr-2" />
 						<span>JK Institute of Applied Physics & Technology, Prayagraj, UP</span>
-					</li>					
+					</li>
 					<li className="flex items-center">
 						<Phone className="w-5 h-5 mr-2" />
 						<span>+91-8967997966</span>
-					</li>					
+					</li>
 					<li className="flex items-center">
 						<Mail className="w-5 h-5 mr-2" />
 						<a href="mailto:biswassagar927@gmail.com" target="_blank">
-						biswassagar927@gmail.com
+							biswassagar927@gmail.com
 						</a>
 					</li>
 					<li className="flex items-center">
@@ -76,13 +76,14 @@ export default function Footer() {
 							Shoping Place
 						</a>
 						<span className='mx-1'>built by</span>
-						<a href="#" target="_blank" className='border-b-2 border-green-500'>
+						<a href="https://www.linkedin.com/in/mrsagarbiswas/" target="_blank" className='border-b-2 border-green-500'>
 							Sagar Biswas
 						</a>
+
 					</li>
 				</ul>
 				<div className="mt-6">
-					Cash On Delivery (COD) Available
+					<Link to="https://drive.google.com/file/d/1U5AUd1l4uYWvDWKNypp4XpDej91Qzu2A/view?usp=sharing" target="_blank">View Project Report</Link>
 				</div>
 			</div>
 		</footer>
